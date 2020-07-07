061f3a4610a543bfd421a75f8bb0614c5f34e8bf
github username <github email address>
2020-07-06 21:07:52 -0400

    first get request

--- src/script.js
+++ src/script.js
@@ -1,1 +1,43 @@
-console.log('Just Heads up');
+document.addEventListener('DOMContentLoaded', () => {
+
+ ARTIST_URL = "http://localhost:3000/api/v1/artists"
+ ALBUM_URL = "http://localhost:3000/api/v1/albums"
+ SONG_URL = "http://localhost:3000/api/v1/songs"
+ COMMENT_URL = "http://localhost:3000/api/v1/comments"
+ fetchArtists()
+
+    function fetchArtists(){
+        fetch(ARTIST_URL)
+        .then(resp => resp.json())
+        .then(artists => {getArtists(artists)})
+    }
+
+    function getArtists(artists){
+        artists.forEach(artist => { 
+            let artistList = document.getElementById("artist-list")
+            let li = document.createElement("li")
+            li.dataset.id = artist.id
+            li.innerText = artist.name
+            li.addEventListener("click", e => {
+                let id = e.target.dataset.id
+                fetchAlbums(id)
+            })
+            artistList.append(li)
+        })
+        
+    }
+
+    function fetchAlbums(){
+        fetch(ALBUM_URL)
+        .then(resp => resp.json())
+        .then(console.log)
+    }
+
+    function getAlbums(){
+
+    }
+
+
+
+
+})
